47c76bca565ed78e6ef2bfb681ec95ca3337542c
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2024-02-09 18:24:02 +0300

    refactoring

--- weather/config/urls.py
+++ weather/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('weather_site.urls'))
+    path('', include('weather_api.urls'))
 ]
--- weather/weather_api/apps.py
+++ weather/weather_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WeatherSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weather_site'
+    name = 'weather_api'
